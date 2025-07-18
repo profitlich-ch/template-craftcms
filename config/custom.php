36eb4f85fdc4b9c58c08c47cc1841a187b695cfd
Moritz Profitlich <m.profitlich@profitlich.ch>
2025-07-18 21:23:05 +0300

    Sass function size() nimmt neu auch mehrere Werte

--- config/custom.php
+++ config/custom.php
@@ -16,7 +16,7 @@
         
         // production environment
         'production'  => array(
-            'staticAssetsVersion' => 1752862330,
+            'staticAssetsVersion' => 1752862968,
         ),
 
         // staging environment
