4a1982ea25eb6b43aa2b71a528297f88efc4b38f
Moritz Profitlich <m.profitlich@profitlich.ch>
2025-07-19 13:18:53 +0300

    Bugfix Sass fonts

--- config/custom.php
+++ config/custom.php
@@ -16,7 +16,7 @@
         
         // production environment
         'production'  => array(
-            'staticAssetsVersion' => 1752915248,
+            'staticAssetsVersion' => 1752920322,
         ),
 
         // staging environment
